2145f832ed2eea8f75767c3e295622826a09b999
KevinSant1998 <131640238+KevinSant1998@users.noreply.github.com>
2024-06-20 09:54:55 -0600

    Cambios Nombre ViewModels

--- SARIS/Models/Clacificacion_CI/Clacificacion_CIViewModel.cs
+++ SARIS/Models/Clacificacion_CI/Clacificacion_CIViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,8 @@ namespace OrionCoreCableColor.Models.Clacificacion_CI
 {
     public class Clacificacion_CIViewModel
     {
+        [Display(Name = "Clasificación CI")]
+        [Required]
         public int fiIDCI { get; set; }
         public string fcCI { get; set; }
         public bool EsEditar { get; internal set; }
--- SARIS/Models/Region/RegionViewModel.cs
+++ SARIS/Models/Region/RegionViewModel.cs
@@ -13,7 +13,7 @@ namespace OrionCoreCableColor.Models.Region
         public int fIIDPais { get; set; }
         public string fcPais { get; set; }
         public int fiIDRegion { get; set; }
-        [Display(Name = "Pais")]
+        [Display(Name = "Region")]
         [Required]
         public string fcRegion { get; set; }
         public bool EsEditar { get; internal set; }
