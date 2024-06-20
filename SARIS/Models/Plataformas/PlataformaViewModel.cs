42101c9069050933dc10d8ef6f00fcbab846a631
KevinSant1998 <131640238+KevinSant1998@users.noreply.github.com>
2024-06-20 14:35:15 -0600

    Plataformas

---
+++ SARIS/Models/Plataformas/PlataformaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrionCoreCableColor.Models.Plataformas
+{
+    public class PlataformaViewModel
+    {
+        public int fiIDPlataforma { get; set; }
+        public string fcNombrePlataforma { get; set; }
+        public string fcDescripcionPlataforma { get; set; }
+        public Nullable<int> fiActivo { get; set; }
+        public bool EsEditar { get; internal set; }
+    }
+}
