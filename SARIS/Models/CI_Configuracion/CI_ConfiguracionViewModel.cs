e013c8ade35e3f8bc5d5dd59b868c8c904f5c850
KevinSant1998 <131640238+KevinSant1998@users.noreply.github.com>
2024-06-20 09:43:13 -0600

    cambios

--- SARIS/Models/CI_Configuracion/CI_ConfiguracionViewModel.cs
+++ SARIS/Models/CI_Configuracion/CI_ConfiguracionViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,16 +8,30 @@ namespace OrionCoreCableColor.Models.CI_Configuracion
 {
     public class CI_ConfiguracionViewModel
     {
+        [Display(Name = "Pais")]
+        [Required]
         public Nullable<int> fiIDPais { get; set; }
         public string fcPais { get; set; }
+        [Display(Name = "Region")]
+        [Required]
         public Nullable<int> fiIDRegion { get; set; }
         public string fcRegion { get; set; }
+        [Display(Name = "Ciudad")]
+        [Required]
         public Nullable<int> fiIDCiudad { get; set; }
         public string fcCiudad { get; set; }
+        [Display(Name = "Clasificacion CI")]
+        [Required]
         public Nullable<int> fiIDCI { get; set; }
         public int fiIDConfiguracion { get; set; }
+        [Display(Name = "CI Configuracion")]
+        [Required]
         public string fcConfiguracion { get; set; }
+        [Display(Name = "Latitud")]
+        [Required]
         public string fcLatitud { get; set; }
+        [Display(Name = "Longitud")]
+        [Required]
         public string fcLongitud { get; set; }
         public string fcCI { get; set; }
         public bool EsEditar { get; set; }
